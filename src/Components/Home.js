c21d3c10a6b18ed74c47669f64d8e113e2c22603
Anjanay Raina <anjanayraina326@gmail.com>
2022-09-09 17:30:59 +0530

    Home Button Added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { MDBBtn } from 'mdb-react-ui-kit';
+import '../Styles/Home.css'
 function Home() {
   return (
-    <div>This is the Homme</div>
+    <>
+<div class="buttons">
+  <div class="container">
+     
+      <h1>Welcome To Tech Wars</h1>
+      <a  className="btn effect01" ><span>Connect Metamask</span></a>
+  </div>
+</div>
+    </>
   )
 }
 
