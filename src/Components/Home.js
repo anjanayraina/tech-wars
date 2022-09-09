3f2a09515141fc36c92fea3f4776a1cc8d05f228
Anjanay Raina <anjanayraina326@gmail.com>
2022-09-09 17:44:05 +0530

    Basic Styling Added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import { MDBBtn } from 'mdb-react-ui-kit';
+import { Container, Row, Col } from "react-bootstrap";
 import '../Styles/Home.css'
 function Home() {
   return (
     <>
+
+    <Container fluid className = "Header">
+        <h1>Welcome To Tech Wars</h1>
+    </Container>
+
+    <Container fluid>
 <div class="buttons">
   <div class="container">
-     
-      <h1>Welcome To Tech Wars</h1>
+
       <a  className="btn effect01" ><span>Connect Metamask</span></a>
   </div>
 </div>
+</Container>
     </>
   )
 }
