c76caaeded6b270098c9d8bb8c283016a737056d
Anjanay Raina <anjanayraina326@gmail.com>
2022-09-13 13:22:29 +0530

    Main Page Created , Styling Left

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React  ,{useState}from 'react'
 import {useLocation} from 'react-router-dom';
+import '../Styles/MainPage.css'
+const NavBar = () => (
+  <div className='navbar'>
+      <div className='navbar__title navbar__item'>Tech Wars</div>
+      <div className='navbar__item'>About Us</div>
+      <div className='navbar__item'>Contact</div>
+      <div className='navbar__item'>Help</div>        
+  </div>
+);
 function MainPage() {
 
   const location = useLocation();
   let wallet = location.state.walletAddress;
+  const [showBasic, setShowBasic] = useState(false);
   console.log(wallet);
   return (
-    <div><h1>this is the playing screen : {}</h1></div>
+   <div>
+   <NavBar />
+
+   </div>
   )
 }
 
