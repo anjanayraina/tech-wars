d4943bfd177b6a024b1e2d6a0f1b93f9fe558277
Anjanay Raina <anjanayraina326@gmail.com>
2022-09-16 12:00:10 +0530

    Battle.js Created

---
+++ src/Components/Battle/Battle.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Battle() {
+  return (
+    <div className =  "wrapper">
+
+    <div className = "player">This is a Player</div>
+    <div className = "opponent">This is the Opponent</div>
+
+    </div>
+  )
+}
+
+export default Battle
